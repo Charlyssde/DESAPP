27d55363508a2615d0df017000ee08e0be1030a3
Aline Montserrat <alinemhdez@gmail.com>
2019-11-14 16:54:31 -0600

    Pantallas
    Solo las pantallas iniciar sesión y registrar usuario

---
+++ Codigo/app/src/main/java/com/photogram/Feed/Feed.java
@@ -0,0 +1,16 @@
+package com.photogram.Feed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.photogram.R;
+
+public class Feed extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+    }
+}
