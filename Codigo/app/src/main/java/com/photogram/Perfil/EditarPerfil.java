b08f4117c389abeed72130d4b9d5791c27889bb2
Aline Montserrat <alinemhdez@gmail.com>
2019-12-04 23:01:35 -0600

    Pantalla editar perfil

---
+++ Codigo/app/src/main/java/com/photogram/Perfil/EditarPerfil.java
@@ -0,0 +1,16 @@
+package com.photogram.Perfil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.photogram.R;
+
+public class EditarPerfil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_perfil);
+    }
+}
